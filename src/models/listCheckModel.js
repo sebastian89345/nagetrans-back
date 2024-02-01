c183b1c8bf46c0420077de219c5449c4001a6329
juan sebastian <juan.quiroz@gse.com.co>
2024-02-01 06:46:14 -0500

    new changes list check

--- src/models/listCheckModel.js
+++ src/models/listCheckModel.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const listCheckSchema = new Schema({
+    
     userVehicle: [{
         ref:"users",
         type:Schema.Types.ObjectId
@@ -10,38 +11,55 @@ const listCheckSchema = new Schema({
         ref:"users",
         type:Schema.Types.ObjectId
     }],
+    // preguntar
     date:String,
     oilChange:String,
-    currentKm:String,
-    engineOilIndicator:String,
-    fuelLevel:String,
-    whistle:String,
-    batteryIndicator:String,
-    emergencyBrake:String,
-    chairCushions:String,
-    wiperWasher:String,
-    internalLights:String,
-    instrumentSpeedometerDashboard:String,
-    engineOil:String,
-    hydraulicOilSteering:String,
-    coolantLiquid:String,
-    brakeFluid:String,
-    fuelcap:String,
-    beltTension:String,
-    mirrorGlass:String,
-    highLowBeams:String,
-    turnSignals:String,
-    logoPlates:String,
-    tires:String,
-    deviceSpeed:String,
-    safetyBelts:String,
-    firstaidkit:String,
-    extinguisher:String,
-    dateExtinguisherExpiration:String,
-    roadTeam:String,
-    spareTire:String,
+    currentKm:String,    
     observation:String,
-    largeTent:String
+    dateExtinguisherExpiration:String,
+    //revision Interna
+    wiperWasher:String,
+    emergencyBrake:String,
+    whistle:String,
+    safetyBelts:String,
+    GlassLifter:String, 
+    Lightning:String, 
+    //Revisión Externa
+    directionals:String,
+    Stationary:String,
+    highBeams:String,
+    lowLights:String,
+    stop:String,
+    reverse:String, 
+    battery:String,
+    tireWear:String,
+    tireCondition:String,
+    tireAirPressure:String,
+    //Motor
+    engineLeaks:String, 
+    brakeLeaks:String, 
+    beltTension:String,
+    wetFilters:String, 
+    //Niveles
+    motorOil:String,
+    transmissionOil:String,
+    refrigerant:String,
+    windshieldWiperWater:String,
+    radiatorAdditives:String,
+    roadTeam:String,
+    firstAidKit:String,
+    //Kit Herramientas
+    extinguisher:String,
+    crossPiece:String,
+    cat:String,
+    tacos:String,
+    signs:String,
+    vest:String,
+    flashlight:String,
+    //Elementos de bioseguridad
+    gloveHolder:String,
+    wearYourFaceMask:String,
+    antibacterialGelOrAlcoholHolder:String,        
 },{
     timestamps: true,
     versionKey: false
