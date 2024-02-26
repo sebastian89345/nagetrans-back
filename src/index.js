ffda0a284c8e82aa8b4dd97df3fbed832e1613d8
juan sebastian <juan.quiroz@gse.com.co>
2024-02-26 04:50:24 -0500

    new changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 const app = express(); 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const v1TypeRoutes = require('./routes/typeRoutes');
